f38d307f031cb7aaa40921259f4a9e0ac79bbca9
railgun-0402 <april-life.f-441-2011@docomo.ne.jp>
2024-10-21 02:13:30 +0900

    ホテル一覧ページ&Unit Test

---
+++ src/main/java/com/example/samuraitravel/controller/AdminHouseController.java
@@ -0,0 +1,29 @@
+package com.example.samuraitravel.controller;
+
+import com.example.samuraitravel.entity.House;
+import com.example.samuraitravel.service.HouseService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/admin/houses")
+public class AdminHouseController {
+    private final HouseService houseService;
+
+    /* Constructor */
+    public AdminHouseController(HouseService houseService) {
+        this.houseService = houseService;
+    }
+
+    @GetMapping
+    public String index(Model model) {
+        List<House> houses = houseService.findAllHouses();
+        model.addAttribute("houses", houses);
+
+        return "admin/houses/index";
+    }
+}
