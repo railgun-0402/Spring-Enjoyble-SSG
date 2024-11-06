e444e053c74861f3cf424cc169c19a57fccdee39
railgun-0402 <april-life.f-441-2011@docomo.ne.jp>
2024-11-07 00:37:34 +0900

    house-db

--- src/main/java/com/example/samuraitravel/service/HouseService.java
+++ src/main/java/com/example/samuraitravel/service/HouseService.java
@@ -1,13 +1,22 @@
 package com.example.samuraitravel.service;
 
 import com.example.samuraitravel.entity.House;
+import com.example.samuraitravel.form.HouseRegisterForm;
 import com.example.samuraitravel.repository.HouseRepository;
 import org.springframework.data.domain.Page;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class HouseService {
@@ -31,5 +40,49 @@ public class HouseService {
     /* Get Houses by ID */
     public Optional<House> findHouseById(Integer id) {
         return houseRepository.findById(id);
+    }
+
+    @Transactional
+    public void createHouse(HouseRegisterForm houseRegisterForm) {
+        House house = new House();
+        MultipartFile imageFile = houseRegisterForm.getImageFile();
+
+        if (!imageFile.isEmpty()) {
+            String imageName = imageFile.getOriginalFilename();
+            String hashedImageName = generateNewFileName(imageName);
+            Path filePath = Paths.get("src/main/resources/static/storage/" + hashedImageName);
+            copyImageFile(imageFile, filePath);
+            house.setImageName(hashedImageName);
+        }
+
+        house.setName(houseRegisterForm.getName());
+        house.setDescription(houseRegisterForm.getDescription());
+        house.setPrice(houseRegisterForm.getPrice());
+        house.setCapacity(houseRegisterForm.getCapacity());
+        house.setPostalCode(houseRegisterForm.getPostalCode());
+        house.setAddress(houseRegisterForm.getAddress());
+        house.setPhoneNumber(houseRegisterForm.getPhoneNumber());
+
+        houseRepository.save(house);
+    }
+
+    // UUIDで生成ファイル名を返す
+    public String generateNewFileName(String imageName) {
+        String[] fileNames = imageName.split("\\.");
+
+        for (int i = 0; i < fileNames.length - 1; i++) {
+            fileNames[i] = UUID.randomUUID().toString();
+        }
+
+        return String.join(".", fileNames);
+    }
+
+    // 画像ファイルを指定したファイルにコピーする
+    public void copyImageFile(MultipartFile imageFile, Path filePath) {
+        try {
+            Files.copy(imageFile.getInputStream(), filePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
