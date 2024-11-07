5b45f8bb83efc1f66e192ce0e38eca379b53677a
railgun-0402 <april-life.f-441-2011@docomo.ne.jp>
2024-11-08 01:44:09 +0900

    民宿登録DBの処理&Unit Test2

--- src/test/java/com/example/samuraitravel/controller/AdminHouseControllerTest.java
+++ src/test/java/com/example/samuraitravel/controller/AdminHouseControllerTest.java
@@ -1,5 +1,6 @@
 package com.example.samuraitravel.controller;
 
+import com.example.samuraitravel.entity.House;
 import com.example.samuraitravel.service.HouseService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,7 +24,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@ActiveProfiles("ci")
+@ActiveProfiles("test")
 public class AdminHouseControllerTest {
     /* Testでは簡潔性を優先し、field injection */
     @Autowired
@@ -135,5 +136,91 @@ public class AdminHouseControllerTest {
 
         // レコード数が変わっていないことを検証する
         assertThat(countAfter).isEqualTo(countBefore);
+    }
+
+    @Test
+    @WithUserDetails("taro.samurai@example.com")
+    @Transactional
+    public void 一般ユーザーとしてログイン済みの場合は民宿を登録せずに403エラーが発生する() throws Exception {
+        // テスト前のレコード数を取得する
+        long countBefore = houseService.countHouses();
+
+        // テスト用の画像ファイルデータを準備する
+        Path filePath = Paths.get("src/main/resources/static/storage/house01.jpg");
+        String fileName = filePath.getFileName().toString();
+        String fileType = Files.probeContentType(filePath);
+        byte[] fileBytes = Files.readAllBytes(filePath);
+
+        MockMultipartFile imageFile = new MockMultipartFile(
+                "imageFile",  // フォームのname属性の値
+                fileName,     // ファイル名
+                fileType,     // ファイルの形式
+                fileBytes     // ファイルのバイト配列
+        );
+
+        mockMvc.perform(MockMvcRequestBuilders.multipart("/admin/houses/create").file(imageFile)
+                        .with(csrf())
+                        .param("name", "テスト民宿名")
+                        .param("description", "テスト説明")
+                        .param("price", "5000")
+                        .param("capacity", "5")
+                        .param("postalCode", "000-0000")
+                        .param("address", "テスト住所")
+                        .param("phoneNumber", "000-000-000"))
+                .andExpect(status().isForbidden());
+
+        // テスト後のレコード数を取得する
+        long countAfter = houseService.countHouses();
+
+        // レコード数が変わっていないことを検証する
+        assertThat(countAfter).isEqualTo(countBefore);
+    }
+
+    @Test
+    @WithUserDetails("hanako.samurai@example.com")
+    @Transactional
+    public void 管理者としてログイン済みの場合は民宿登録後に民宿一覧ページにリダイレクトする() throws Exception {
+        // テスト前のレコード数を取得する
+        long countBefore = houseService.countHouses();
+
+        // テスト用の画像ファイルデータを準備する
+        Path filePath = Paths.get("src/main/resources/static/storage/house01.jpg");
+        String fileName = filePath.getFileName().toString();
+        String fileType = Files.probeContentType(filePath);
+        byte[] fileBytes = Files.readAllBytes(filePath);
+
+        MockMultipartFile imageFile = new MockMultipartFile(
+                "imageFile",  // フォームのname属性の値
+                fileName,     // ファイル名
+                fileType,     // ファイルの形式
+                fileBytes     // ファイルのバイト配列
+        );
+
+        mockMvc.perform(MockMvcRequestBuilders.multipart("/admin/houses/create").file(imageFile)
+                        .with(csrf())
+                        .param("name", "テスト民宿名")
+                        .param("description", "テスト説明")
+                        .param("price", "5000")
+                        .param("capacity", "5")
+                        .param("postalCode", "000-0000")
+                        .param("address", "テスト住所")
+                        .param("phoneNumber", "000-000-000"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/admin/houses"));
+
+        // テスト後のレコード数を取得する
+        long countAfter = houseService.countHouses();
+
+        // レコード数が1つ増加していることを検証する
+        assertThat(countAfter).isEqualTo(countBefore + 1);
+
+        House house = houseService.findFirstHouseByOrderByIdDesc();
+        assertThat(house.getName()).isEqualTo("テスト民宿名");
+        assertThat(house.getDescription()).isEqualTo("テスト説明");
+        assertThat(house.getPrice()).isEqualTo(5000);
+        assertThat(house.getCapacity()).isEqualTo(5);
+        assertThat(house.getPostalCode()).isEqualTo("000-0000");
+        assertThat(house.getAddress()).isEqualTo("テスト住所");
+        assertThat(house.getPhoneNumber()).isEqualTo("000-000-000");
     }
 }
